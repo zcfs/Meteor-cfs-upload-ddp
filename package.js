46356b9b083f4c2ea1cca245c0aca859d5f63a74
Morten Henriksen <mh@gi2.dk>
2014-12-17 13:50:14 +0100

    mbr update versions and fix warnings

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 Package.describe({
+  git: 'https://github.com/CollectionFS/Meteor-cfs-upload-ddp.git',
   name: 'cfs:upload-ddp',
   version: '0.0.1',
   summary: 'CollectionFS, DDP File Upload'
 });
 
-Package.on_use(function(api) {
-  api.versionsFrom('METEOR@0.9.1');
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
 
   api.use([
     //CFS packages
-    'cfs:base-package@0.0.0',
+    'cfs:base-package@0.0.26',
     'cfs:tempstore@0.0.2',
     'cfs:file@0.0.0',
     'cfs:ejson-file@0.0.0',
@@ -21,14 +22,14 @@ Package.on_use(function(api) {
     'ejson',
     //Other packages
     'cfs:power-queue@0.0.1',
-    'cfs:reactive-list@0.0.0'
+    'cfs:reactive-list@0.0.8'
     ]);
 
-  api.add_files([
+  api.addFiles([
     'upload-ddp-client.js'
     ], 'client');
 
-  api.add_files([
+  api.addFiles([
     'upload-ddp-server.js'
     ], 'server');
 });
@@ -39,6 +40,6 @@ Package.on_use(function(api) {
 //   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
 //    'random', 'deps']);
 
-//   api.add_files('tests/server-tests.js', 'server');
-//   api.add_files('tests/client-tests.js', 'client');
+//   api.addFiles('tests/server-tests.js', 'server');
+//   api.addFiles('tests/client-tests.js', 'client');
 // });
