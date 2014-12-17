79e793cc8be9531bf123e0ee05a4b09ebbc54362
Morten Henriksen <mh@gi2.dk>
2014-12-17 21:02:50 +0100

    bump versions

--- package.js
+++ package.js
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use([
     //CFS packages
     'cfs:base-package@0.0.27',
-    'cfs:tempstore@0.1.1',
+    'cfs:tempstore@0.1.2',
     'cfs:file@0.1.14',
-    'cfs:ejson-file@0.1.2',
+    'cfs:ejson-file@0.1.3',
     //Core packages
     'deps',
     'check',
